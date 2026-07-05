d42587c45bba82e17dc16abab2819ec037225f53
Roma <cherepanov@gmail.com>
2026-07-05 19:20:31 +0000

    chore: release v1.4.0 (Music/Apps/Settings redesign + default-launcher fix)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "online.k73.bmwlauncher"
         minSdk = 26
         targetSdk = 33
-        versionCode = 15
-        versionName = "1.3.1"
+        versionCode = 16
+        versionName = "1.4.0"
     }
     signingConfigs {
         create("release") {
