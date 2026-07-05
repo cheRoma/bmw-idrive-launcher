77353d5f1b050105611034eaad74a0b55beb573f
Roma <cherepanov@gmail.com>
2026-07-05 14:19:12 +0000

    feat(home): add RibbonClock time + Russian date

---
+++ app/src/test/java/online/k73/bmwlauncher/ui/home/RibbonClockTest.kt
@@ -0,0 +1,18 @@
+package online.k73.bmwlauncher.ui.home
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.time.LocalDateTime
+
+class RibbonClockTest {
+    @Test fun time_is_hh_mm_zero_padded() {
+        assertEquals("19:42", RibbonClock.time(LocalDateTime.of(2026, 7, 5, 19, 42)))
+        assertEquals("07:05", RibbonClock.time(LocalDateTime.of(2026, 7, 5, 7, 5)))
+    }
+    @Test fun date_is_russian_short_dow_day_month() {
+        // 2026-07-05 is a Sunday
+        assertEquals("Вс, 5 июля", RibbonClock.date(LocalDateTime.of(2026, 7, 5, 0, 0)))
+        // 2026-01-01 is a Thursday
+        assertEquals("Чт, 1 января", RibbonClock.date(LocalDateTime.of(2026, 1, 1, 0, 0)))
+    }
+}
